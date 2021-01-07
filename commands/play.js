02088fd65af7c4524df3078036cc1f554a5af519
M <chanon_w@cmu.ac.th>
2021-01-07 18:41:33 +0700

    fix delete message

--- commands/play.js
+++ commands/play.js
@@ -1,65 +1,64 @@
-const { play } = require("../include/play");
-const ytdl = require("ytdl-core");
-const YouTubeAPI = require("simple-youtube-api");
-const scdl = require("soundcloud-downloader").default;
-const https = require("https");
-const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID, DEFAULT_VOLUME } = require("../util/EvobotUtil");
-const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
-const { MessageEmbed } = require("discord.js");
+const { play } = require('../include/play')
+const ytdl = require('ytdl-core')
+const YouTubeAPI = require('simple-youtube-api')
+const scdl = require('soundcloud-downloader').default
+const https = require('https')
+const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID, DEFAULT_VOLUME } = require('../util/EvobotUtil')
+const youtube = new YouTubeAPI(YOUTUBE_API_KEY)
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
-  name: "play",
+  name: 'play',
   cooldown: 3,
-  aliases: ["p"],
-  description: "Plays audio from YouTube or Soundcloud",
+  aliases: ['p'],
+  description: 'Plays audio from YouTube or Soundcloud',
   async execute(message, args) {
-    const { channel } = message.member.voice;
+    const { channel } = message.member.voice
 
-    const serverQueue = message.client.queue.get(message.guild.id);
-    if (!channel) return message.reply("You need to join a voice channel first!").catch(console.error);
+    const serverQueue = message.client.queue.get(message.guild.id)
+    if (!channel) return message.reply('You need to join a voice channel first!').catch(console.error)
     if (serverQueue && channel !== message.guild.me.voice.channel)
-      return message.reply(`You must be in the same channel as ${message.client.user}`).catch(console.error);
+      return message.reply(`You must be in the same channel as ${message.client.user}`).catch(console.error)
 
     if (!args.length)
       return message
         .reply(`Usage: ${message.client.prefix}play <YouTube URL | Video Name | Soundcloud URL>`)
-        .catch(console.error);
+        .catch(console.error)
 
-    const permissions = channel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT"))
-      return message.reply("Cannot connect to voice channel, missing permissions");
-    if (!permissions.has("SPEAK"))
-      return message.reply("I cannot speak in this voice channel, make sure I have the proper permissions!");
+    const permissions = channel.permissionsFor(message.client.user)
+    if (!permissions.has('CONNECT')) return message.reply('Cannot connect to voice channel, missing permissions')
+    if (!permissions.has('SPEAK'))
+      return message.reply('I cannot speak in this voice channel, make sure I have the proper permissions!')
 
-    const search = args.join(" ");
-    const videoPattern = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
-    const playlistPattern = /^.*(list=)([^#\&\?]*).*/gi;
-    const scRegex = /^https?:\/\/(soundcloud\.com)\/(.*)$/;
-    const mobileScRegex = /^https?:\/\/(soundcloud\.app\.goo\.gl)\/(.*)$/;
-    const url = args[0];
-    const urlValid = videoPattern.test(args[0]);
+    const search = args.join(' ')
+    const videoPattern = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi
+    const playlistPattern = /^.*(list=)([^#\&\?]*).*/gi
+    const scRegex = /^https?:\/\/(soundcloud\.com)\/(.*)$/
+    const mobileScRegex = /^https?:\/\/(soundcloud\.app\.goo\.gl)\/(.*)$/
+    const url = args[0]
+    const urlValid = videoPattern.test(args[0])
 
     // Start the playlist if playlist url was provided
     if (!videoPattern.test(args[0]) && playlistPattern.test(args[0])) {
-      return message.client.commands.get("playlist").execute(message, args);
-    } else if (scdl.isValidUrl(url) && url.includes("/sets/")) {
-      return message.client.commands.get("playlist").execute(message, args);
+      return message.client.commands.get('playlist').execute(message, args)
+    } else if (scdl.isValidUrl(url) && url.includes('/sets/')) {
+      return message.client.commands.get('playlist').execute(message, args)
     }
 
     if (mobileScRegex.test(url)) {
       try {
         https.get(url, function (res) {
-          if (res.statusCode == "302") {
-            return message.client.commands.get("play").execute(message, [res.headers.location]);
+          if (res.statusCode == '302') {
+            return message.client.commands.get('play').execute(message, [res.headers.location])
           } else {
-            return message.reply("No content could be found at that url.").catch(console.error);
+            return message.reply('No content could be found at that url.').catch(console.error)
           }
-        });
+        })
       } catch (error) {
-        console.error(error);
-        return message.reply(error.message).catch(console.error);
+        console.error(error)
+        return message.reply(error.message).catch(console.error)
       }
-      return message.reply("Following url redirection...").catch(console.error);
+      return message.reply('Following url redirection...').catch(console.error)
     }
 
     const queueConstruct = {
@@ -70,80 +69,78 @@ module.exports = {
       loop: false,
       volume: DEFAULT_VOLUME || 100,
       playing: true
-    };
+    }
 
-    let songInfo = null;
-    let song = null;
+    let songInfo = null
+    let song = null
 
     if (urlValid) {
       try {
-        songInfo = await ytdl.getInfo(url);
+        songInfo = await ytdl.getInfo(url)
         song = {
           title: songInfo.videoDetails.title,
           url: songInfo.videoDetails.video_url,
           duration: songInfo.videoDetails.lengthSeconds
-        };
-        let items = songInfo.related_videos.filter((item) => item.length_seconds < 600);
-        let item = items[Math.floor(Math.random() * items.length)];
-        console.log(items, items);
-        message.client.autoplayID = item.id;
+        }
+        let items = songInfo.related_videos.filter((item) => item.length_seconds < 600)
+        let item = items[Math.floor(Math.random() * items.length)]
+        // console.log(items, items);
+        message.client.autoplayID = item.id
       } catch (error) {
-        console.error(error);
-        return message.reply(error.message).catch(console.error);
+        console.error(error)
+        return message.reply(error.message).catch(console.error)
       }
     } else if (scRegex.test(url)) {
       try {
-        const trackInfo = await scdl.getInfo(url, SOUNDCLOUD_CLIENT_ID);
+        const trackInfo = await scdl.getInfo(url, SOUNDCLOUD_CLIENT_ID)
         song = {
           title: trackInfo.title,
           url: trackInfo.permalink_url,
           duration: Math.ceil(trackInfo.duration / 1000)
-        };
+        }
       } catch (error) {
-        console.error(error);
-        return message.reply(error.message).catch(console.error);
+        console.error(error)
+        return message.reply(error.message).catch(console.error)
       }
     } else {
       try {
-        const results = await youtube.searchVideos(search, 1);
-        songInfo = await ytdl.getInfo(results[0].url);
-        let items = songInfo.related_videos.filter((item) => item.length_seconds < 600);
-        let item = items[Math.floor(Math.random() * items.length)];
-        console.log(items, items);
-        message.client.autoplayID = item.id;
+        const results = await youtube.searchVideos(search, 1)
+        songInfo = await ytdl.getInfo(results[0].url)
+        let items = songInfo.related_videos.filter((item) => item.length_seconds < 600)
+        let item = items[Math.floor(Math.random() * items.length)]
+        // console.log(items, items);
+        message.client.autoplayID = item.id
 
         song = {
           title: songInfo.videoDetails.title,
           url: songInfo.videoDetails.video_url,
           duration: songInfo.videoDetails.lengthSeconds
-        };
+        }
       } catch (error) {
-        console.error(error);
-        return message.reply(error.message).catch(console.error);
+        console.error(error)
+        return message.reply(error.message).catch(console.error)
       }
     }
 
     if (serverQueue) {
-      serverQueue.songs.push(song);
+      serverQueue.songs.push(song)
 
-      const embed = new MessageEmbed()
-        .setColor("PURPLE")
-        .setDescription(`${song.title} has been added to the queue!`);
-      return message.channel.send(embed).catch(console.error);
+      const embed = new MessageEmbed().setColor('PURPLE').setDescription(`${song.title} has been added to the queue!`)
+      return message.channel.send(embed).catch(console.error)
     }
 
-    queueConstruct.songs.push(song);
-    message.client.queue.set(message.guild.id, queueConstruct);
+    queueConstruct.songs.push(song)
+    message.client.queue.set(message.guild.id, queueConstruct)
 
     try {
-      queueConstruct.connection = await channel.join();
-      await queueConstruct.connection.voice.setSelfDeaf(false);
-      play(queueConstruct.songs[0], message);
+      queueConstruct.connection = await channel.join()
+      await queueConstruct.connection.voice.setSelfDeaf(false)
+      play(queueConstruct.songs[0], message)
     } catch (error) {
-      console.error(error);
-      message.client.queue.delete(message.guild.id);
-      await channel.leave();
-      return message.channel.send(`Could not join the channel: ${error}`).catch(console.error);
+      console.error(error)
+      message.client.queue.delete(message.guild.id)
+      await channel.leave()
+      return message.channel.send(`Could not join the channel: ${error}`).catch(console.error)
     }
   }
-};
+}
